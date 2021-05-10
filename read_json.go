407c96e4f3a56bbd028b6231126faf7520f764b2
Alex Rosa <h.rosa822@gmail.com>
2021-05-09 20:56:03 -0600

    Update write function

--- read_json.go
+++ read_json.go
@@ -17,6 +17,17 @@ type Device struct {
 	State int    `json:"state"`
 }
 
+func (d Devices) describeData() {
+	for i := 0; i < len(d.Devices); i++ {
+		fmt.Println("Device Type: " + d.Devices[i].Name)
+		fmt.Println("state: " + strconv.Itoa(d.Devices[i].State))
+	}
+}
+
+func (d *Device) updateState(desiredState int) {
+	d.State = desiredState
+}
+
 func main() {
 
 	// Open the json file
@@ -38,9 +49,12 @@ func main() {
 
 	json.Unmarshal(byteValue, &devices)
 
-	for i := 0; i < len(devices.Devices); i++ {
-		fmt.Println("Device Type: " + devices.Devices[i].Name)
-		fmt.Println("state: " + strconv.Itoa(devices.Devices[i].State))
-	}
+	devices.describeData()
+	devices.Devices[0].updateState(0)
+	fmt.Println("changing device state")
+	devices.describeData()
+
+	updatedData, _ := json.MarshalIndent(devices, "", "    ")
+	err = ioutil.WriteFile("output.json", updatedData, 0644)
 
 }
